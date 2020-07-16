b63b69af8b00e63cfd04f4fe887a59de75203b6f
MANISH <manishchandrashekharan@gmail.com>
2020-07-16 19:58:47 +0530

    use case 9 created

--- sortingaritcomputation.sh
+++ sortingaritcomputation.sh
@@ -47,4 +47,21 @@ do
 done
 echo ${Array[@]}
 
+#uc9
+for ((i = 0; i<4; i++)) 
+do
+      
+    for((j = 0; j<4-i-1; j++)) 
+    do
+      
+        if [ ${Array[j]} -gt ${Array[$((j+1))]} ] 
+        then
+            # swap 
+            temp=${Array[j]} 
+            Array[$j]=${Array[$((j+1))]}   
+            Array[$((j+1))]=$temp 
+        fi
+    done
+done
+echo ${Array[@]}
 
